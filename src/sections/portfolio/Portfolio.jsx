226b1bbb5a0946547020ef69796e915bfa41be4e
kespinak <kespinak@gmail.com>
2023-02-15 11:14:29 -0800

    added prettier formating

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,36 +1,42 @@
-import './portfolio.css'
-import data from './data'
-import ProjectsCategories from './ProjectsCategories'
-import Projects from './Projects'
-import React, {useState} from 'react'
+import "./portfolio.css";
+import data from "./data";
+import ProjectsCategories from "./ProjectsCategories";
+import Projects from "./Projects";
+import React, { useState } from "react";
 
 const Portfolio = () => {
   const [projects, setProjects] = useState(data);
 
-  const categories = data.map(item => item.category);
-  const uniqueCategories = ['all', ...new Set(categories)];
+  const categories = data.map((item) => item.category);
+  const uniqueCategories = ["all", ...new Set(categories)];
   // console.log(categories, uniqueCategories);
   const filterProjectHandler = (category) => {
-    if(category === 'all') {
+    if (category === "all") {
       setProjects(data);
       return;
     }
-    const filterProjects = data.filter(project => project.category === category);
-    setProjects(filterProjects)
-  }
+    const filterProjects = data.filter(
+      (project) => project.category === category
+    );
+    setProjects(filterProjects);
+  };
 
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
       <p>
-        Check out some of the projects I recently worked on. Use the buttons to toggle the different categories.
+        Check out some of the projects I recently worked on. Use the buttons to
+        toggle the different categories.
       </p>
       <div className="container portfolio__container">
-        <ProjectsCategories categories={uniqueCategories} onFilterProjects={filterProjectHandler} />
-        <Projects projects={projects}/>
+        <ProjectsCategories
+          categories={uniqueCategories}
+          onFilterProjects={filterProjectHandler}
+        />
+        <Projects projects={projects} />
       </div>
     </section>
   );
-}
+};
 
-export default Portfolio
+export default Portfolio;
